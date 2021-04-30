ee99e32bd775cb7a5914e1f2fe12aa4281584a77
Vladimir <vova.myltsin@yandex.ru>
2021-04-30 13:40:38 +0400

    4

--- dailyPass.js
+++ dailyPass.js
@@ -3,12 +3,12 @@ const {transitionFromJupiterToBackwater, transitionFromPripyatToJupiter} = requi
 const {takeTaskForStrelok} = require('./questions');
 
 async function dailyPass() {
-	await searchSwagPripyat(); // Поиск хабара в Припяти (Готово)
-	await transitionFromPripyatToJupiter(); // Переход на Юпитер (Готово)
-	await searchSwagJupiter(); // Поиск хабара на Юпитере (Готово)
-	await transitionFromJupiterToBackwater(); // Переход на Затон (Готово)
-	await searchSwagBackwater(); // Поиск хабара на Затоне (Готово)
-	await searchSwagDone(); // Сдача хабара Вобле (Готово)
+	await searchSwagPripyat(); // Поиск хабара в Припяти
+	await transitionFromPripyatToJupiter(); // Переход на Юпитер
+	await searchSwagJupiter(); // Поиск хабара на Юпитере
+	await transitionFromJupiterToBackwater(); // Переход на Затон
+	await searchSwagBackwater(); // Поиск хабара на Затоне
+	await searchSwagDone(); // Сдача хабара Вобле
 }
 
 module.exports = {
